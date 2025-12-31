d270c56e8371a02eef590825c9e539c22c64db47
arunprajapati-arch <arunprajapati.mail@gmail.com>
2025-12-31 15:27:43 +0530

    added redis as queue

--- apps/uploadService/src/index.ts
+++ apps/uploadService/src/index.ts
@@ -4,10 +4,12 @@ import simpleGit from 'simple-git';
 import { createId } from '../utils/cuid';
 import { uploadFolderToS3 } from '../utils/uploadS3';
 const app = express();
+import { redis, initRedis } from "@repo/redis";
 
 app.use(cors());
 app.use(express.json());
 
+await initRedis();
 
 
 app.post("/deploy", async (req, res) => {
@@ -17,6 +19,7 @@ app.post("/deploy", async (req, res) => {
     if (!id) res.json("Internal Error")
     await simpleGit().clone(repoUrl, `./output/${id}`)
     await uploadFolderToS3(id);
+    await redis.lPush("build-queue", id);
     res.json(id);
 })
 
---
+++ packages/redis/src/index.ts
@@ -0,0 +1,7 @@
+import { createClient } from "redis";
+
+export const redis = createClient({ url: "redis://localhost:6379" });
+
+export const initRedis = async () => {
+    if (!redis.isOpen) await redis.connect();
+}
